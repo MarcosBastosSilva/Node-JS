9414816ae7b250449a81ec1a283925ae0c72e4d0
Marcos Vinicius Bastos da Silva <bastosmarcos90@gmail.com>
2022-09-26 18:06:06 -0300

    update

--- exercicio2/index.js
+++ exercicio2/index.js
@@ -3,7 +3,19 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req,res){
-    res.end("Gerenciador Financeiro")
+    res.send("Gerenciador Financeiro")
+})
+
+app.get("/sobre-empresa", function(req,res){
+    res.send("Pagina sobre a empresa")
+})
+
+app.get("/blog", function(req,res){
+    res.send("Pagina sobre o blog")
+})
+
+app.get("/contato", function(req,res){
+    res.send("Pagina de contato")
 })
 
 app.listen(8080)
